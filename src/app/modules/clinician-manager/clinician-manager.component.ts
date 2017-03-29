dde8e331915e9375ba40540aecec261e1190dd78
Jan Hreno <jan.hreno@gmail.com>
2017-03-29 22:04:51 +0200

    date picker changed

--- src/app/modules/clinician-manager/clinician-manager.component.ts
+++ src/app/modules/clinician-manager/clinician-manager.component.ts
@@ -1,27 +1,38 @@
 import { Component, OnInit }    from '@angular/core';
 import { Router }               from '@angular/router';
 import {NgDateRangePickerOptions} from "ng-daterangepicker";
+import {IMyOptions, IMyDateRangeModel} from 'mydaterangepicker';
+
 
 @Component({
     templateUrl: 'clinician-manager.component.html'
 })
 export class ClinicianManagerComponent implements OnInit {
 
-    options: NgDateRangePickerOptions;
+    private options: NgDateRangePickerOptions;
+
+    private myDateRangePickerOptions: IMyOptions;
+
+    private model: any;
 
     constructor( ) { }
 
     ngOnInit(): void {
+        // Initialized to specific date (09.10.2018).
+        this.model = {date: {year: 2017, month: 10, day: 9}};
 
-        this.options = {
-            theme: 'default',
-            range: 'tm',
-            dayNames: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-            presetNames: ['This Month', 'Last Month', 'This Week', 'Last Week', 'This Year', 'Last Year', 'Start', 'End'],
-            dateFormat: 'yMd',
-            outputFormat: 'DD.MM.YYYY',
-            startOfWeek: 1
+        this.myDateRangePickerOptions = {
+            // other options...
+            dateFormat: 'dd.mm.yyyy',
+            firstDayOfWeek: "mo"
+
         };
         
+    }
+
+    onDateRangeChanged(event: IMyDateRangeModel) {
+        console.log('onDateRangeChanged(): Begin date: ', event.beginDate, ' End date: ', event.endDate);
+        console.log('onDateRangeChanged(): Formatted: ', event.formatted);
+        console.log('onDateRangeChanged(): BeginEpoc timestamp: ', event.beginEpoc, ' - endEpoc timestamp: ', event.endEpoc);
     }
 }
