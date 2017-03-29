6388a47c9923585eeb1f0e5206ca1411493121ac
Jan Hreno <jan.hreno@gmail.com>
2017-03-29 17:40:43 +0200

    Clinician Dashboard refactored to Clinician Manager

--- src/app/modules/data-resource-browser/model/patient-check.ts
+++
@@ -1,9 +0,0 @@
-export class PatientCheck {
-  id: string;
-  clinician: string;
-  startDate: Date;
-  endDate: Date;
-  color: string;
-  visitReason: string;
-  //type: string;
-}
--- src/app/modules/data-resource-browser/model/patient-observation.ts
+++
@@ -1,9 +0,0 @@
-
-export class PatientObservation {
-  id: string;
-  value: string;
-  date: Date;
-   dateFormatted () :string {
-    return this.date.toLocaleDateString();
-}
-}
