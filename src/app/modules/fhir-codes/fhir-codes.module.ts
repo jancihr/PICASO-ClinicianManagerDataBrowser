a8caf78d85a8130d6c93a873e1acb0074aa64426
Evangelos Vlachogiannis <evangelos.vlachogiannis@gmail.com>
2017-05-17 12:16:37 +0200

    fix fhir-codes link/route

--- src/app/modules/fhir-codes/fhir-codes.module.ts
+++ src/app/modules/fhir-codes/fhir-codes.module.ts
@@ -1,19 +1,20 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FhirCodesComponent} from './fhir-codes.component';
+import {FhirCodesRoutingModule} from "./fhir-codes-routing.module";
 
 @NgModule({
-    imports: [
-        CommonModule
-    ],
-    declarations: [
-        FhirCodesComponent
-    ],
-    exports: [
-        FhirCodesComponent
-    ],
-    providers:[
-    ]
+  imports: [
+    FhirCodesRoutingModule,
+    CommonModule
+  ],
+  declarations: [
+    FhirCodesComponent
+  ],
+  exports: [
+    FhirCodesComponent
+  ],
+  providers: []
 })
 export class FhirCodesModule {
 
